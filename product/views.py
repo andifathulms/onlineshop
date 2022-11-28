4490cbad1679741438281dd1cdd8516a3800aa25
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2022-11-28 08:33:08 +0800

    product list finishing

--- product/views.py
+++ product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
-from .models import Product
+from .models import Product, SubCategory
 from .utils import populated_context_subcategory
 
 class SubCategoryViews(View):
@@ -10,6 +10,7 @@ class SubCategoryViews(View):
         context = {}
         populated_context_subcategory(context)
 
+        context["subcategory"] = SubCategory.objects.get(name=kwargs['type'])
         context["products"] = Product.objects.filter(sub_category__name=kwargs['type'])
         
         return render(request, 'product/product_list.html', context)
